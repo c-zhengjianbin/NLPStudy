7c60ce104f2cfaa71e1734f6957590b1affdbfcd
c-zhengjianbin <615893265@qq.com>
2019-11-10 10:43:41 +0800

    first commit

---
+++ src/main/java/com/zhengjianbin/nlp/NlptestApplication.java
@@ -0,0 +1,13 @@
+package com.zhengjianbin.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlptestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhengjianbin/nlp/NlptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengjianbin.nlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
